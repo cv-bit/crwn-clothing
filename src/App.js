6f3702cb2a9a73920fcbe058b1a74b4d82360c06
cv-bit <ververcameron29@gmail.com>
2022-06-21 00:20:51 -0500

    adding directory

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import './categories.styles.scss'
-import CategoryItem from './components/category-item/category-item.component';
+import Directory from './components/directory/directory.component';
 import categories from './components/category-menu';
 
 function App() {
 
   return (
-    <div className="categories-container">
-      {categories.map((category) => {
-        return <CategoryItem key={category.id} category={category}/>
-      })}
-    </div>
+    <Directory categories={categories} />
   );
 }
 
